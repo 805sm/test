dcf6aed4fd652557ce4606ac67ed3b7e03d99661
tjdanqkr <49061542+tjdanqkr@users.noreply.github.com>
2019-06-10 20:08:54 +0900

    Revert "테스트 입니다."
    This reverts commit 474112cdacccba782636c5a3802b032c71fbd5d2.

--- andong1/src/interfaceex/Area.java
+++
@@ -1,6 +0,0 @@
-package interfaceex;
-//final static 항상붙어있음
-public interface Area extends Exam2{
-	int Max= 100;
-	public abstract void disp();
-}
--- andong1/src/interfaceex/Exam2.java
+++
@@ -1,7 +0,0 @@
-package interfaceex;
-
-public interface Exam2 {
-	int Max= 100;
-	public abstract void disp1();
-
-}
